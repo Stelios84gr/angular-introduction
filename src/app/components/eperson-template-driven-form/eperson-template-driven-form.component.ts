7ecc36cc1ae837aca0a670cd1e85517da933df61
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-05-05 17:45:08 +0300

    libraries added

--- src/app/components/eperson-template-driven-form/eperson-template-driven-form.component.ts
+++ src/app/components/eperson-template-driven-form/eperson-template-driven-form.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms'; // NgForm: για να "μάθει" η κλάση τα data ενός πεδίου form
+import { MatSelectModule } from '@angular/material/select'; // για μορφοποίηση πεδίων τύπου select
+import { MatInputModule } from '@angular/material/input'; // για μορφοποίηση πεδίων τύπου input
+import { MatFormFieldModule } from '@angular/material/form-field';  // για μορφοποίηση πεδίων τύπου field
+import { MatButtonModule } from '@angular/material/button'; // για μορφοποίηση πεδίων τύπου button
 
 @Component({
   selector: 'app-eperson-template-driven-form',
-  imports: [],
+  imports: [ FormsModule, MatSelectModule, MatInputModule, MatFormFieldModule, MatButtonModule],
   templateUrl: './eperson-template-driven-form.component.html',
   styleUrl: './eperson-template-driven-form.component.css'
 })
--- src/app/components/template-driven-forms-example/template-driven-forms-example.component.ts
+++ src/app/components/template-driven-forms-example/template-driven-forms-example.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { EpersonTemplateDrivenFormComponent } from '../eperson-template-driven-form/eperson-template-driven-form.component';
+import { PersonTableComponent } from '../person-table/person-table.component';
+import { SimpleDatatableComponent } from '../simple-datatable/simple-datatable.component';
 
 @Component({
   selector: 'app-template-driven-forms-example',
-  imports: [],
+  imports: [EpersonTemplateDrivenFormComponent, SimpleDatatableComponent, PersonTableComponent],
   templateUrl: './template-driven-forms-example.component.html',
   styleUrl: './template-driven-forms-example.component.css'
 })
