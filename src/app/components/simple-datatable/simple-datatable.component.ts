59dd30691db942863183f7e16b29cdd2df822c99
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-05-12 19:11:45 +0300

    codefix at eperson-reactive-form component

--- src/app/components/simple-datatable/simple-datatable.component.ts
+++ src/app/components/simple-datatable/simple-datatable.component.ts
@@ -1,21 +1,28 @@
-import { Component, EventEmitter, Input, Output, effect, inject } from '@angular/core';
+import { Component, EventEmitter, Input, Output, SimpleChanges, effect, inject } from '@angular/core';
 import { EPerson } from 'src/app/shared/interfaces/eperson';
 import {sortBy} from 'lodash-es';
-import { PersonService } from 'src/app/shared/services/person.service';
+// import { PersonService } from 'src/app/shared/services/person.service';
 
 @Component({
   selector: 'app-simple-datatable',
   imports: [],
   templateUrl: './simple-datatable.component.html',
-  styleUrl: './simple-datatable.component.css'
+  styleUrl: './simple-datatable.component.css',
 })
 export class SimpleDatatableComponent {
   @Input() data: EPerson[] | undefined; // δέχεται data από parent component
-  @Output() personClicked = new EventEmitter<EPerson>() // στέλνει data με δομή EPerson σε parent component
+  @Output() personClicked = new EventEmitter<EPerson>(); // στέλνει data με δομή EPerson σε parent component
 
-  personService = inject(PersonService);
+  // personService = inject(PersonService);
 
-  epersonsData: EPerson[] | undefined = [];
+  epersonsData: EPerson[] = []; // δεν χρειάζεται | undefined λόγω της if παρακάτω
+
+  // χρησιμοποιείται όταν έχουμε @Input για να δείξει πως έχουν γίνει αλλαγές στο data και να γίνουν κάποιες διαδικασίες - changes: SimpleChanges: όταν γίνονται αλλαγές στο data, να γίνει το περιεχόμενο της μεθόδου
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['data'] && this.data) {
+      this.epersonsData = this.data;
+    }
+  }
 
   // constructor(){
   //   effect(() => {
@@ -32,20 +39,14 @@ export class SimpleDatatableComponent {
     surName: 'none',
     age: 'none',
     email: 'none',
-    education: 'none'
-  }
-
- ngOnInit() {
-  console.log("NgOnInit")
-  this.epersonsData = this.data;
- }
+    education: 'none',
+  };
 
   sortData(sortKey: keyof EPerson): void {
     // console.log(sortKey);'
-    this.epersonsData = this.data;
-    console.log("1>>>", this.epersonsData)
+
     if (this.sortOrder[sortKey] === 'asc') {
-      this.sortOrder[sortKey] = 'desc'
+      this.sortOrder[sortKey] = 'desc';
       this.epersonsData = sortBy(this.data, sortKey).reverse();
     } else {
       this.sortOrder[sortKey] = 'asc';
@@ -53,22 +54,23 @@ export class SimpleDatatableComponent {
     }
 
     for (let key in this.sortOrder) {
-      if (key!==sortKey) {  // αν το εκάστοτε key του sortOrder δεν είναι το sortKey, παίρνει τιμή 'none'
-        this.sortOrder[key as keyof EPerson] = 'none'
+      if (key !== sortKey) {
+        // αν το εκάστοτε key του sortOrder δεν είναι το sortKey, παίρνει τιμή 'none'
+        this.sortOrder[key as keyof EPerson] = 'none';
       }
     }
 
-    console.log("Simple DataaTable", this.data);
+    console.log('Simple DataaTable', this.data);
   }
 
   sortSign(sortKey: keyof EPerson): string {
-    if (this.sortOrder[sortKey] === 'asc') return '\u2191'
-    else if (this.sortOrder[sortKey] === 'desc') return '\u2193'
+    if (this.sortOrder[sortKey] === 'asc') return '\u2191';
+    else if (this.sortOrder[sortKey] === 'desc') return '\u2193';
     else return '';
   }
 
   onPersonClicked(person: EPerson) {
-    console.log("Person>>", person)
-    this.personClicked.emit(person);  // ενεργοποιώντας το event personClicked στέλνει person
+    console.log('Person>>', person);
+    this.personClicked.emit(person); // ενεργοποιώντας το event personClicked στέλνει person
   }
 }
